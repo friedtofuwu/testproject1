8af24f7ae82cb847b9989529ad0fe336ca310fe8
friedtofuwu <zekunli9334@gmail.com>
2023-09-07 12:02:23 -0400

    1

---
+++ app/src/main/java/edu/vt/cs3714/fall2023/testproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.vt.cs3714.fall2023.testproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.app.Activity
+class MainActivity : AppCompatActivity() {
+    // ImageView img;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
